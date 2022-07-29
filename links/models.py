f5b82b50cccf7633c22f1d7d3b09f6fa3c5b8491
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-07-29 05:47:16 +0100

    Django-Querysets-and-Managers

---
+++ links/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from .managers import ActiveLinkManager
+
+# Create your models here.
+User = get_user_model()
+
+class Link(models.Model):
+    target_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(max_length=20, blank=True, unique= True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField()
+    objects = models.Manager()
+    public = ActiveLinkManager()
+
+    def __str__(self):
+        return self.target_url
